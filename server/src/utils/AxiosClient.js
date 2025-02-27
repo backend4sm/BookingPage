56d943fd59c381e92d00e0bc4ea07300af71b817
backend4sm <backend4@sicilimia.com>
2025-02-27 17:21:24 +0200

    update AxiosClient

--- server/src/utils/AxiosClient.js
+++ server/src/utils/AxiosClient.js
@@ -1,15 +1,75 @@
 import axios from "axios";
 
 const API_URL = process.env.BEDS24_API;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 
 const axiosClient = axios.create({ baseURL: API_URL });
 
-axiosClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.token = token;
+let token = null;
+let refreshing = false;
+let refreshSubscribers = [];
+
+const subscribeTokenRefresh = (cb) => {
+  refreshSubscribers.push(cb);
+};
+
+const onTokenRefreshed = (newToken) => {
+  refreshSubscribers.forEach((cb) => cb(newToken));
+  refreshSubscribers = [];
+};
+
+// Function to refresh token
+const refreshToken = async () => {
+  if (!refreshing) {
+    refreshing = true;
+    try {
+      const response = await axios.get(`${API_URL}authentication/token`, {
+        headers: { refreshToken: REFRESH_TOKEN },
+      });
+
+      token = response.data.token;
+      onTokenRefreshed(token);
+    } catch (error) {
+      console.error("Error refreshing token:", error);
+      throw error;
+    } finally {
+      refreshing = false;
+    }
   }
-  return config;
-});
+
+  return new Promise((resolve) => {
+    subscribeTokenRefresh(resolve);
+  });
+};
+
+// Request interceptor to add token to headers
+axiosClient.interceptors.request.use(
+  async (config) => {
+    console.log(token);
+    if (!token) {
+      token = await refreshToken();
+    }
+    config.headers.token = token;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// Response interceptor to handle token expiration
+axiosClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      try {
+        token = await refreshToken();
+        error.config.headers.token = token;
+        return axiosClient(error.config);
+      } catch (err) {
+        return Promise.reject(err);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axiosClient;
